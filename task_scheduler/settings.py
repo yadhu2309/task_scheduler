3529f15f04829c50d33794ae4908f2b67d89a989
yadhu2309 <yadhu2309@gmail.com>
2024-09-01 19:07:46 +0530

    requirements

--- task_scheduler/settings.py
+++ task_scheduler/settings.py
@@ -100,8 +100,15 @@ WSGI_APPLICATION = 'task_scheduler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'events',
+        'USER': 'admin',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
